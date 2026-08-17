5d3ee5a7ef62a531f40caaa0e59216d34c1be105
Lin Zewei <linzewei39@gmail.com>
2026-08-17 17:29:49 +0800

    Rename chatbot from Duke to Zucc
    Keep the Java entry point, startup banner, setup instructions, and user guide consistent with the new chatbot name.

---
+++ src/main/java/Zucc.java
@@ -0,0 +1,14 @@
+/**
+ * Starts the Zucc chatbot application.
+ */
+public class Zucc {
+    public static void main(String[] args) {
+        String banner = " ______                \n"
+                + "|___  /                \n"
+                + "   / / _   _  ___ ___  \n"
+                + "  / / | | | |/ __/ __| \n"
+                + " / /__| |_| | (_| (__  \n"
+                + "/_____|\\__,_|\\___\\___|\n";
+        System.out.println(banner);
+    }
+}
